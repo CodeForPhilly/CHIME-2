c256439b3d6930d83373a6bbea09efddc54c3e40
BrianThomasRoss <ML@brianthomasross.com>
2020-05-22 05:36:32 -0400

    User DTO

---
+++ src/chime2-web/app/main/dto/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Data Transfer Objects
+---------------------
+
+These define the schema of the data
+"""
+__all__ = ["UserDto", "AuthDto"]
+
+from .user import UserDto, AuthDto
