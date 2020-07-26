035e2319aa231d89ac185a8c2b8832b865b9d8a8
Brian Ross <ml@brianthomasross.com>
2020-07-25 20:50:06 -0400

    Overload dash styling

--- app/dashboard/customization.py
+++ app/dashboard/customization.py
@@ -12,7 +12,8 @@ nav_html = load_html("nav.html").split(
 )[0]
 footer_html = load_html("footer.html")
 
-custom_index_str = """<!DOCTYPE html>
+custom_index_str = """
+<!DOCTYPE html>
 <html>
     <head>
         {%metas%}
@@ -20,16 +21,51 @@ custom_index_str = """<!DOCTYPE html>
         {%favicon%}
         {%css%}
     </head>
-    <body>
-        {%app_entry%}
-        {%footer%}
-        <footer>
-            {%config%}
-            {%scripts%}
-            {%renderer%}
-        </footer>
-    </body>
-</html>"""
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="https://www.pennmedicine.org/">
+            <img class="container"
+                 src="https://www.pennmedicine.org/Assets/PennMedicine/built/images/assets/logo-white.svg">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">CHIME 2
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <!--Secondary navbar links -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/dashboard/">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contributors/">Contributors</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about/">Guide</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about/">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="jumbotron" style="padding-top:100px">
+        <body>
+            {%app_entry%}
+            <footer>
+                {%config%}
+                {%scripts%}
+                {%renderer%}
+            </footer>
+        </body>
+    </div>
+</html>
+"""
 
 
 class CustomDash(dash.Dash):
